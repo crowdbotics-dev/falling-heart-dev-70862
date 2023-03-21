4e8c660cd6704ef52f78787bb14e5a20582ca2a3
Crowdbotics <engineering@crowdbotics.com>
2023-03-21 00:19:10 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "falling-heart-dev-70862",
+  url: "https://falling-heart-dev-70862.botics.co",
+  api: "https://falling-heart-dev-70862.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
